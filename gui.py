af172466bec2fb49e54b82a8fb675438c29e7552
Mathieu La Brie <95377226+Toaster545@users.noreply.github.com>
2025-01-26 16:36:04 -0500

    Product blocks

--- gui.py
+++ gui.py
@@ -1,7 +1,8 @@
 import PySimpleGUI as sg
 import sys
+from algos import solve
 
-def createGui(po_df):
+def createGui(po_df, inv_df):
 # create the gui for the application start
 
     selected_items = []
@@ -9,10 +10,10 @@ def createGui(po_df):
     
     layout = [
     [sg.Text("Select an option from the dropdown:")],
-    [sg.Combo(po_list, key='-DROPDOWN-', enable_events=True)],
+    [sg.Combo(po_list, key='-DROPDOWN-',default_value=po_list[0], enable_events=True)],
     [sg.Button("Add")], 
     [sg.Listbox(values=selected_items, size=(30, 6), key='-LISTBOX-')],
-    [sg.Button("Exit")]
+    [sg.Button("Submit"), sg.Button("Exit")]
 ]
     window = sg.Window("LabelEdge Optimiser", layout, size=(800, 600))
 
@@ -27,4 +28,7 @@ def createGui(po_df):
             if selected_option not in selected_items:
                 selected_items.append(selected_option)  # Add to the list
                 window['-LISTBOX-'].update(selected_items)  # Update the Listbox
+        
+        if event == "Submit":
+            value = solve(inv_df=inv_df, po_df=po_df ,selected_pos=selected_items)
     window.close()
