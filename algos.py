9321ad21ba742bde11833d76fc6ac241a4fcd114
Mathieu La Brie <95377226+Toaster545@users.noreply.github.com>
2025-02-11 14:50:38 -0500

    Label Code choice

--- algos.py
+++ algos.py
@@ -1,6 +1,5 @@
 def createProductBlocks(po_df, selected_pos):
     
-    print(selected_pos)
     numbers = []
     for p in selected_pos:
         p = p.split(' ')[0]
@@ -24,11 +23,33 @@ def createProductBlocks(po_df, selected_pos):
     
     return valid_products_per_row
 
+def createMasterDict(inv_df):
+    
+    masterDict = []
+    
+    for index, row in inv_df.iterrows():
+        width = float(row['Larg.'])
+        length = float(row['Longueur'])
+        tempDict = {'Width': width, 'Length': length, 'Cur Total': 0, }
+        masterDict.append(tempDict)
+    
+    
+    return masterDict
+
 def initialSol():
     return
 
-def solve(inv_df, po_df, selected_pos):
-    products = createProductBlocks(po_df, selected_pos)
+def solve(inv_df, po_df, selected_pos, label_code):
+    keys = ['Paper', 'Width', 'Length', 'Nb', 'msi']
+    products_dict = [dict(zip(keys, item.split('/'))) for item in selected_pos]
+    products = [(float(d['Width']), float(d['Length'])) for d in products_dict for _ in range(int(float(d['Nb'])))]
+    total_msi_pos = sum(float(d['msi']) for d in products_dict)
+    print("Total MSI: ", total_msi_pos , "Products: ", products)
+    
+    inv_df = inv_df.loc[inv_df['Code LabelEdge'] == label_code]
+    inv_df = inv_df.drop(columns=['Code LabelEdge'])
+    masters = createMasterDict(inv_df=inv_df)
+    print("Masters", masters)
     
     
     
